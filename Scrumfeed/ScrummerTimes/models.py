85bb4081c5aaf4aea9be3e4c5c133affc89fc5f3
simoneid <simoneid@stud.ntnu.no>
2018-10-02 13:27:31 +0200

    Added a feed for articles that are not proofread and the feed can only be accessed by users with the adequate permissions

--- Scrumfeed/ScrummerTimes/models.py
+++ Scrumfeed/ScrummerTimes/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+
 from django.db import models
 
 
@@ -7,8 +8,7 @@ class Article(models.Model):
     title = models.CharField(max_length=200,  blank=True)
     header_image = models.ImageField(upload_to='header_image', blank=True)
     text = models.TextField(blank=True)
+    is_read = models.BooleanField(blank=False, default=False)
 
     def __str__(self):
         return self.title.__str__()
-
-
