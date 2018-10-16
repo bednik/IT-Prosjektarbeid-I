08d458ef8093a395ee1bf98db838f6aa3552a27b
Øystein Bjørkeng Haugen <oystebha@stud.ntnu.no>
2018-10-16 21:43:57 +0200

    Kategorier forms

--- Scrumfeed/ScrummerTimes/forms.py
+++ Scrumfeed/ScrummerTimes/forms.py
@@ -6,8 +6,8 @@ from django.core.exceptions import ValidationError
 from django.forms import forms, CharField, IntegerField, ImageField, ChoiceField
 from ScrummerTimes.choices import CATEGORIES
 from django.core.files.base import ContentFile
-from django.forms import forms, CharField, IntegerField, ImageField, URLField, TypedChoiceField, RadioSelect, BooleanField
-from ScrummerTimes.models import Article
+from django.forms import forms, CharField, IntegerField, ImageField, URLField, TypedChoiceField, RadioSelect, BooleanField, ModelChoiceField
+from ScrummerTimes.models import Article, Category
 
 # Noe tull
 class ArticleForm(forms.Form):
@@ -24,7 +24,8 @@ class ArticleForm(forms.Form):
    # required=False
 #)
     text = CharField()
-    category = ChoiceField(choices=CATEGORIES, required=False)
+    #category = ChoiceField(choices=CATEGORIES, required=False)
+    category = ModelChoiceField(queryset=Category.objects.all())
 
     class Meta:
         #The two below has something to do with assigning who the author of the article is
@@ -44,8 +45,8 @@ class ArticleForm(forms.Form):
 
 
 class FilterForm(forms.Form):
-    category = ChoiceField(choices=CATEGORIES)
-
+   # category = ChoiceField(choices=CATEGORIES)
+    category = ModelChoiceField(queryset=Category.objects.all())
     class Meta:
         model = Article
 
@@ -60,3 +61,19 @@ class FilterForm(forms.Form):
         #    raise ValidationError({'name': "Description must be provided"}, code='invalid')
         #return self.cleaned_data
 
+class CreateCategoryForm(forms.Form):
+    category = ModelChoiceField(queryset=Category.objects.all(), required = False)
+    name = CharField(max_length=50, required= False)
+    class Meta:
+        model = Category
+    def clean(self):
+        name = self.cleaned_data['name']
+        #if(name == '' or name == None):
+        #    raise ValidationError({'name': "Your category can not be blank"}, code='invalid')
+        if(' ' in name):
+            #raise ValidationError({'name': "Your category can not have spaces"}, code='invalid')
+            raise forms.ValidationError("Your category can not have spaces", code='invalid')
+        if(Category.objects.filter(name = name).exists()):
+            raise forms.ValidationError("A category with this name already exists", code='invalid')
+
+        return self.cleaned_data
